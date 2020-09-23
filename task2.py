e7e8bfe80d84ff5c05525579517ea22e044071cd
BrueShavrk <borisshavrin@yandex.ru>
2020-09-23 22:52:12 +0300

    modified task2
    1. Исправил декоратор @classmethod на @abstractmethod
    2. Изменил конструктор для передачи параметров роста и размера

--- task2.py
+++ task2.py
@@ -6,36 +6,50 @@
     для костюма (2 * H + 0.3). Проверить работу этих методов на реальных данных.
 Реализовать общий подсчет расхода ткани. Проверить на практике полученные на этом уроке знания:
     реализовать абстрактные классы для основных классов проекта, проверить на практике работу декоратора @property."""
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
-class Clothes(ABC):
-    def __init__(self, param):
-        self.param = param
+class Clothes(ABC):                 # базовый класс Одежда
+    def __init__(self, name):
+        self._name = name
 
-    @classmethod                    # создание абстрактного метода
+    @property
+    def name(self):
+        return self._name
+
+    @abstractmethod                 # создание абстрактного метода
     def consumption(self):
         pass
 
 
-class Coat(Clothes):
-    @property
-    def consumption(self):
-        consumption = self.param / 6.5 + 0.5
-        return consumption
+class Coat(Clothes):                # класс Пальто
+    def __init__(self, name, size):
+        super().__init__(name)
+        self.__size = size
+
+    def consumption(self):          # метод расчета расхода ткани для Пальто
+        consumption_coat = self.__size / 6.5 + 0.5
+        return consumption_coat
 
 
-class Suit(Clothes):
-    @property
-    def consumption(self):
-        consumption = 2 * self.param + 0.3
-        return consumption
+class Suit(Clothes):                # класс Костюм
+    def __init__(self, name, height):
+        super().__init__(name)
+        self.__height = height
+
+    def consumption(self):          # метод расчета расхода ткани для Пальто
+        consumption_suit = 2 * self.__height + 0.3
+        return consumption_suit
 
 
-coat = Coat(100)
-suit = Suit(180)
+def print_consumption(res):
+    print('Расход ткани для {}, составлет: {:.2f}'.format(res.name, res.consumption()))
 
-print('Расход ткани для пальто {}-го размера, составлет: {:.2f}'.format(coat.param, coat.consumption))
-print('Расход ткани для костюма (рост {}см) составляет: {:.2f}'.format(suit.param, suit.consumption))
 
-print('Общий подсчет расхода ткани: {:.2f}'.format(coat.consumption + suit.consumption))
+coat1 = Coat('Пальто', 53)
+suit1 = Suit('Костюм', 180)
+print_consumption(coat1)
+print_consumption(suit1)
+
+"""Расчет общего расхода ткани"""
+print('Общий расход ткани: {:.2f}'.format(coat1.consumption() + suit1.consumption()))
