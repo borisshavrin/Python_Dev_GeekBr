fe024cef6e71c2a6ad419582482e1b6d08d7c445
BrueShavrk <borisshavrin@yandex.ru>
2020-09-23 22:04:38 +0300

    modified task1
    1. Убрал генерацию матриц в конструкторе. 
    Теперь на ввод принимается сгенерированная отдельно матрица.
    
    2. Исправил метод str. Теперь матрица выводится построчно и с пробелами.

--- task1.py
+++ task1.py
@@ -9,24 +9,37 @@ import random
 
 
 class Matrix:
-    def __init__(self, row, column):            # конструктор класса, принимающий кол-во строк и колонок матрицы
-        self.row = row
-        self.column = column
-        self.matrix = [[random.randrange(10) for j in range(column)] for i in range(row)]   # генерир-е матрицы
+    def __init__(self, list_numbers):
+        self.matrix = list_numbers
 
-    def __str__(self):                          # метод вывода матрицы
-        return f'{self.matrix}'
+    def __str__(self):                          # метод перевода матрицы в текстовый формат
+        str_matrix = ''                         # создание пустой строки для внесения в нее чисел
+        for i in self.matrix:
+            for j in i:
+                str_matrix += str('{:<4}'.format(j))     # перевод ряда матрицы в текст (c выравниванием)
+            str_matrix += '\n'                  # перенос ряда на новую строку
+        return str_matrix
 
     def __add__(self, other):                   # метод сложения матриц
         new_matrix = self.matrix                # присваивание 1-й матрицы
         for i in range(len(self.matrix)):               # обход индексов строк и колонок
             for j in range(len(self.matrix[i])):
-                new_matrix[i][j] += other.matrix[i][j]      # прибавление элементов 2-й матрицы того же индекса
-        return f'{new_matrix}'
+                new_matrix[i][j] += other.matrix[i][j]  # прибавление элементов остальных матриц того же индекса
+        return Matrix(new_matrix)
 
 
-matrix_one = Matrix(5, 5)
-matrix_two = Matrix(5, 5)
-print(matrix_one)
-print(matrix_two)
-print(matrix_two + matrix_one)
+matrix_one = [[random.randrange(10) for j in range(5)] for i in range(3)]
+matrix_two = [[random.randrange(10) for k in range(5)] for l in range(3)]
+# matrix_three = [[random.randrange(10) for n in range(5)] for m in range(3)]
+# matrix_four = [[random.randrange(10) for p in range(5)] for o in range(3)]
+
+a = Matrix(matrix_one)
+b = Matrix(matrix_two)
+# c = Matrix(matrix_three)
+# d = Matrix(matrix_four)
+
+print(a)
+print(b)
+# print(c)
+# print(d)
+print(a + b)    # также работает с 3-4мя матрицами
